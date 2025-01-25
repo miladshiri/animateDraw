557e9973d4ba2a2965bbca41c5e65c36896c0e9d
Milad Shiri <miladonlinemail@gmail.com>
2025-01-25 12:05:44 +0200

    fix issue with resizing

--- app/page.js
+++ app/page.js
@@ -190,6 +190,7 @@ export default function Home() {
 
   const handleMouseDownBottomRight = (e) => {
     e.preventDefault();
+    e.stopPropagation();
     isResizing.current = true;
 
     // Store initial container and mouse positions
