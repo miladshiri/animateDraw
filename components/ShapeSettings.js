c45e2a19ef5644eb174eb8638784014b6569a533
miladshiri <miladonlinemail@gmail.com>
2025-04-18 23:40:57 +0300

    Add gradient animation to text, and some clean coding

--- components/ShapeSettings.js
+++ components/ShapeSettings.js
@@ -3,39 +3,38 @@ import { HexColorPicker } from "react-colorful";
 import ColorPickerComponent from "./ColorPickerComponent";
 import { motion } from "framer-motion";
 
-const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPalette, colorPalette}) => {
+const ShapeSettings = ({ selectedShape, changeShapeSettingByName, updateColorPalette, colorPalette }) => {
+  const [animationSpeed, setAnimationSpeed] = useState(selectedShape.settings?.animationSpeed);
+  const [arrowHeads, setArrowHeads] = useState(selectedShape.settings?.head);
+  const [shapeText, setShapeText] = useState(selectedShape.settings?.shapeText);
+  const [thickness, setThickness] = useState(selectedShape.settings?.thickness);
+  const [textAnimation, setTextAnimation] = useState(selectedShape.settings?.textAnimation);
+  const [typewriterText, setTypewriterText] = useState("");
+  const currentIndexRef = useRef(0);
 
-  const [animationSpeed, setAnimationSpeed] = useState(selectedShape.settings?.animationSpeed)
-  const [arrowHeads, setArrowHeads] = useState(selectedShape.settings?.head)
-  const [shapeText, setShapeText] = useState(selectedShape.settings?.shapeText)
-  const [thickness, setThickness] = useState(selectedShape.settings?.thickness)
-  const [textAnimation, setTextAnimation] = useState(selectedShape.settings?.textAnimation)
-  const [typewriterText, setTypewriterText] = useState("")
-  const currentIndexRef = useRef(0)
-
-  useEffect(()=> {
+  useEffect(() => {
     changeShapeSettingByName("animationSpeed", animationSpeed);
-  }, [animationSpeed])
+  }, [animationSpeed]);
 
-  useEffect(()=> {
+  useEffect(() => {
     changeShapeSettingByName("head", arrowHeads);
-  }, [arrowHeads])
+  }, [arrowHeads]);
 
-  useEffect(()=> {
+  useEffect(() => {
     changeShapeSettingByName("shapeText", shapeText);
-  }, [shapeText])
+  }, [shapeText]);
 
-  useEffect(()=> {
+  useEffect(() => {
     changeShapeSettingByName("thickness", thickness);
-  }, [thickness])
+  }, [thickness]);
 
-  useEffect(()=> {
+  useEffect(() => {
     changeShapeSettingByName("textAnimation", textAnimation);
-  }, [textAnimation])
+  }, [textAnimation]);
 
   useEffect(() => {
     const text = "Type";
-    
+
     const typewriterInterval = setInterval(() => {
       if (currentIndexRef.current < text.length) {
         const nextChar = text[currentIndexRef.current];
@@ -54,26 +53,21 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
     return () => clearInterval(typewriterInterval);
   }, []);
 
-  const handleMouseDown = (event) => {
-    event.stopPropagation();
-  };
-
-  const handleMouseUp = (event) => {
-    event.stopPropagation();
-  };
+  const handleMouseDown = (event) => event.stopPropagation();
+  const handleMouseUp = (event) => event.stopPropagation();
 
   if (!selectedShape.settings.animationSpeedOff) {
-    selectedShape.settings.animationSpeedOff = 'yes'
+    selectedShape.settings.animationSpeedOff = 'yes';
   }
 
   const getAnimationStyle = (type) => {
-    switch(type) {
+    switch (type) {
       case 'color-fade':
         return {
-          animate: { 
+          animate: {
             opacity: [1, 0.2, 1]
           },
-          transition: { 
+          transition: {
             duration: 2,
             repeat: Infinity,
             ease: "easeInOut"
@@ -81,10 +75,10 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         };
       case 'shake':
         return {
-          animate: { 
+          animate: {
             x: [0, -15, 15, -15, 15, 0]
           },
-          transition: { 
+          transition: {
             duration: 0.5,
             repeat: Infinity,
             ease: "easeInOut"
@@ -92,10 +86,10 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         };
       case 'pulse':
         return {
-          animate: { 
+          animate: {
             scale: [0.7, 1.5, 0.7]
           },
-          transition: { 
+          transition: {
             duration: 1,
             repeat: Infinity,
             ease: "easeInOut"
@@ -103,14 +97,29 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         };
       case 'typewriter':
         return {
-          animate: { 
+          animate: {
             width: ['0%', '100%']
           },
-          transition: { 
+          transition: {
             duration: 2,
             repeat: Infinity,
             repeatDelay: 1,
             ease: "linear"
+          }
+        };
+      case 'gradient':
+        return {
+          animate: { backgroundPosition: ['0% 50%', '100% 50%', '0% 50%'] },
+          transition: {
+            duration: 3,
+            repeat: Infinity,
+            ease: "linear"
+          },
+          style: {
+            background: 'linear-gradient(270deg, #ff6ec4, #7873f5, #4facfe)',
+            backgroundSize: '600% 600%',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent'
           }
         };
       default:
@@ -124,12 +133,11 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
     >
-
       {selectedShape.settings.shapeColor && (
         <div className="toolbar-item">
           <div className="toolbar-title">Shape Color</div>
           <div className="toolbar-setting">
-            <ColorPickerComponent initialColor={selectedShape.settings.shapeColor}  changeShapeSettingByName={changeShapeSettingByName} settingName={"shapeColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette} />
+            <ColorPickerComponent initialColor={selectedShape.settings.shapeColor} changeShapeSettingByName={changeShapeSettingByName} settingName={"shapeColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette} />
           </div>
         </div>
       )}
@@ -138,7 +146,7 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         <div className="toolbar-item">
           <div className="toolbar-title">Border Color</div>
           <div className="toolbar-setting">
-            <ColorPickerComponent initialColor={selectedShape.settings.borderColor} changeShapeSettingByName={changeShapeSettingByName} settingName={"borderColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette}/>
+            <ColorPickerComponent initialColor={selectedShape.settings.borderColor} changeShapeSettingByName={changeShapeSettingByName} settingName={"borderColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette} />
           </div>
         </div>
       )}
@@ -147,16 +155,16 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         <div className="toolbar-item">
           <div className="toolbar-title">Flow Color</div>
           <div className="toolbar-setting">
-            <ColorPickerComponent initialColor={selectedShape.settings.flowColor} changeShapeSettingByName={changeShapeSettingByName} settingName={"flowColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette}/>
+            <ColorPickerComponent initialColor={selectedShape.settings.flowColor} changeShapeSettingByName={changeShapeSettingByName} settingName={"flowColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette} />
           </div>
         </div>
       )}
-    
+
       {selectedShape.settings.textColor && (
         <div className="toolbar-item">
           <div className="toolbar-title">Text Color</div>
           <div className="toolbar-setting">
-            <ColorPickerComponent initialColor={selectedShape.settings.textColor} changeShapeSettingByName={changeShapeSettingByName} settingName={"textColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette}/>
+            <ColorPickerComponent initialColor={selectedShape.settings.textColor} changeShapeSettingByName={changeShapeSettingByName} settingName={"textColor"} updateColorPalette={updateColorPalette} colorPalette={colorPalette} />
           </div>
         </div>
       )}
@@ -165,13 +173,12 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         <div className="toolbar-item">
           <div className="toolbar-title">Animation Speed</div>
           <div className="toolbar-setting">
-            
-          {selectedShape.settings.animationSpeedOff == 'yes' && (
-            <button onClick={()=> {setAnimationSpeed('no-animation')}} className={animationSpeed === 'no-animation' ? "isSelected" : ""} >Off</button>
-          )} 
-            <button onClick={()=> {setAnimationSpeed('slow')}} className={animationSpeed === 'slow' ? "isSelected" : ""} >Slow</button>
-            <button onClick={()=> {setAnimationSpeed('normal')}} className={animationSpeed === 'normal' ? "isSelected" : ""} >Normal</button>
-            <button onClick={()=> {setAnimationSpeed('fast')}} className={animationSpeed === 'fast' ? "isSelected" : ""} >Fast</button>
+            {selectedShape.settings.animationSpeedOff === 'yes' && (
+              <button onClick={() => { setAnimationSpeed('no-animation') }} className={animationSpeed === 'no-animation' ? "isSelected" : ""} >Off</button>
+            )}
+            <button onClick={() => { setAnimationSpeed('slow') }} className={animationSpeed === 'slow' ? "isSelected" : ""} >Slow</button>
+            <button onClick={() => { setAnimationSpeed('normal') }} className={animationSpeed === 'normal' ? "isSelected" : ""} >Normal</button>
+            <button onClick={() => { setAnimationSpeed('fast') }} className={animationSpeed === 'fast' ? "isSelected" : ""} >Fast</button>
           </div>
         </div>
       )}
@@ -180,13 +187,12 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         <div className="toolbar-item">
           <div className="toolbar-title">Heads</div>
           <div className="toolbar-setting">
-            
-          {selectedShape.settings.animationSpeedOff == 'yes' && (
-            <button onClick={()=> {setArrowHeads('no-head')}} className={arrowHeads === 'no-head' ? "isSelected" : ""} >Off</button>
-          )} 
-            <button onClick={()=> {setArrowHeads('start')}} className={arrowHeads === 'start' ? "isSelected" : ""} >Start</button>
-            <button onClick={()=> {setArrowHeads('end')}} className={arrowHeads === 'end' ? "isSelected" : ""} >End</button>
-            <button onClick={()=> {setArrowHeads('both')}} className={arrowHeads === 'both' ? "isSelected" : ""} >Both</button>
+            {selectedShape.settings.animationSpeedOff === 'yes' && (
+              <button onClick={() => { setArrowHeads('no-head') }} className={arrowHeads === 'no-head' ? "isSelected" : ""} >Off</button>
+            )}
+            <button onClick={() => { setArrowHeads('start') }} className={arrowHeads === 'start' ? "isSelected" : ""} >Start</button>
+            <button onClick={() => { setArrowHeads('end') }} className={arrowHeads === 'end' ? "isSelected" : ""} >End</button>
+            <button onClick={() => { setArrowHeads('both') }} className={arrowHeads === 'both' ? "isSelected" : ""} >Both</button>
           </div>
         </div>
       )}
@@ -195,10 +201,9 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
         <div className="toolbar-item">
           <div className="toolbar-title">Thickness</div>
           <div className="toolbar-setting">
-            
-          <button onClick={()=> {setThickness('narrow')}} className={thickness === 'narrow' ? "isSelected" : ""} >Narrow</button>
-          <button onClick={()=> {setThickness('normal')}} className={thickness === 'normal' ? "isSelected" : ""} >Normal</button>
-          <button onClick={()=> {setThickness('bold')}} className={thickness === 'bold' ? "isSelected" : ""} >Bold</button>
+            <button onClick={() => { setThickness('narrow') }} className={thickness === 'narrow' ? "isSelected" : ""} >Narrow</button>
+            <button onClick={() => { setThickness('normal') }} className={thickness === 'normal' ? "isSelected" : ""} >Normal</button>
+            <button onClick={() => { setThickness('bold') }} className={thickness === 'bold' ? "isSelected" : ""} >Bold</button>
           </div>
         </div>
       )}
@@ -208,32 +213,32 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
           <div className="toolbar-title">Text Animation</div>
           <div className="toolbar-setting" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '5px' }}>
             <div>
-              <button onClick={()=> {setTextAnimation('none')}} className={textAnimation === 'none' ? "isSelected" : ""} >None</button>
+              <button onClick={() => { setTextAnimation('none') }} className={textAnimation === 'none' ? "isSelected" : ""} >None</button>
             </div>
             <div>
-              <motion.button 
-                onClick={()=> {setTextAnimation('color-fade')}} 
+              <motion.button
+                onClick={() => { setTextAnimation('color-fade') }}
                 className={textAnimation === 'color-fade' ? "isSelected" : ""}
                 {...getAnimationStyle('color-fade')}
               >Blink</motion.button>
             </div>
             <div>
-              <motion.button 
-                onClick={()=> {setTextAnimation('shake')}} 
+              <motion.button
+                onClick={() => { setTextAnimation('shake') }}
                 className={textAnimation === 'shake' ? "isSelected" : ""}
                 {...getAnimationStyle('shake')}
               >Shake</motion.button>
             </div>
             <div>
-              <motion.button 
-                onClick={()=> {setTextAnimation('pulse')}} 
+              <motion.button
+                onClick={() => { setTextAnimation('pulse') }}
                 className={textAnimation === 'pulse' ? "isSelected" : ""}
                 {...getAnimationStyle('pulse')}
               >Pulse</motion.button>
             </div>
             <div>
-              <motion.button 
-                onClick={()=> {setTextAnimation('typewriter')}} 
+              <motion.button
+                onClick={() => { setTextAnimation('typewriter') }}
                 className={textAnimation === 'typewriter' ? "isSelected" : ""}
                 style={{
                   position: "relative",
@@ -259,6 +264,15 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
                 </motion.div>
               </motion.button>
             </div>
+            <div>
+              <motion.button
+                onClick={() => { setTextAnimation('gradient') }}
+                className={textAnimation === 'gradient' ? "isSelected" : ""}
+                {...getAnimationStyle('gradient')}
+              >
+                Gradient
+              </motion.button>
+            </div>
           </div>
         </div>
       )}
@@ -270,24 +284,20 @@ const ShapeSettings = ({selectedShape, changeShapeSettingByName, updateColorPale
             <input
               type="text"
               value={shapeText}
-              onChange={(e) => {setShapeText(e.target.value)}}
-              onKeyDown={(e) => {e.stopPropagation()}}
+              onChange={(e) => { setShapeText(e.target.value) }}
+              onKeyDown={(e) => { e.stopPropagation() }}
               style={{
                 backgroundColor: "#555",
                 color: "white",
-                padding: "2px",
-                paddingRight: "4px",
-                paddingLeft: "4px",
+                padding: "2px 4px",
                 borderRadius: "4px"
               }}
             />
           </div>
         </div>
-    )}
-  </div>
-  )
-}
-
-
+      )}
+    </div>
+  );
+};
 
 export default ShapeSettings;
--- components/shapes/SimpleText.js
+++ components/shapes/SimpleText.js
@@ -2,23 +2,23 @@ import { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
 import { defaultSettings } from "../shapeToComponentMapping";
 
-const SimpleText = ({size, shapeSettings}) => {
+const SimpleText = ({ size, shapeSettings }) => {
   const [fontSize, setFontSize] = useState(0);
   const textInputRef = useRef(null);
   const [displayText, setDisplayText] = useState(shapeSettings.text || "");
 
   let speed = 1;
-  if (shapeSettings.animationSpeed === 'slow') {
+  if (shapeSettings.animationSpeed === "slow") {
     speed = 2;
-  } else if (shapeSettings.animationSpeed === 'normal') {
+  } else if (shapeSettings.animationSpeed === "normal") {
     speed = 1;
-  } else if (shapeSettings.animationSpeed === 'fast') {
+  } else if (shapeSettings.animationSpeed === "fast") {
     speed = 0.5;
   }
 
   useEffect(() => {
-    setFontSize(shapeSettings.fontSizeRate * (size.w + size.h) / 2);
-  }, [size, shapeSettings])
+    setFontSize((shapeSettings.fontSizeRate * (size.w + size.h)) / 2);
+  }, [size, shapeSettings]);
 
   useEffect(() => {
     setDisplayText(shapeSettings.text || "");
@@ -27,68 +27,91 @@ const SimpleText = ({size, shapeSettings}) => {
   const getAnimationStyle = () => {
     if (!shapeSettings.textAnimation) return {};
 
-    switch(shapeSettings.textAnimation) {
-      case 'color-fade':
+    switch (shapeSettings.textAnimation) {
+      case "color-fade":
         return {
-          animate: { 
-            opacity: [1, 0.2, 1]
+          animate: {
+            opacity: [1, 0.2, 1],
           },
-          transition: { 
+          transition: {
             duration: 2 * speed,
             repeat: Infinity,
-            ease: "easeInOut"
-          }
-        };
-      case 'shake':
-        return {
-          animate: { 
-            x: [0, -5, 5, -5, 5, 0]
+            ease: "easeInOut",
           },
-          transition: { 
+        };
+      case "shake":
+        return {
+          animate: {
+            x: [0, -5, 5, -5, 5, 0],
+          },
+          transition: {
             duration: 0.5 * speed,
             repeat: Infinity,
-            ease: "easeInOut"
-          }
-        };
-      case 'pulse':
-        return {
-          animate: { 
-            scale: [1, 1.1, 1]
+            ease: "easeInOut",
           },
-          transition: { 
+        };
+      case "pulse":
+        return {
+          animate: {
+            scale: [1, 1.1, 1],
+          },
+          transition: {
             duration: 1 * speed,
             repeat: Infinity,
-            ease: "easeInOut"
-          }
-        };
-      case 'typewriter':
-        return {
-          animate: { 
-            width: ['0%', '100%']
+            ease: "easeInOut",
           },
-          transition: { 
+        };
+      case "typewriter":
+        return {
+          animate: {
+            width: ["0%", "100%"],
+          },
+          transition: {
             duration: 2 * speed,
             repeat: Infinity,
             repeatDelay: 1 * speed,
-            ease: "linear"
-          }
+            ease: "linear",
+          },
         };
       default:
         return {};
     }
   };
 
+  const isGradient = shapeSettings.textAnimation === "gradient";
+
   const scaledText = (
     <div
       ref={textInputRef}
+      className={isGradient ? "gradient-text" : ""}
       style={{
         fontSize: "16px",
         whiteSpace: "pre",
-        transform: `scale(${size.w / (textInputRef.current?.scrollWidth ) || 1}, ${size.h / (textInputRef.current?.scrollHeight ) || 1  })`,
+        transform: `scale(${
+          size.w / (textInputRef.current?.scrollWidth || 1)
+        }, ${size.h / (textInputRef.current?.scrollHeight || 1)})`,
         overflow: "hidden",
-        color: shapeSettings.textColor,
-        WebkitTextStroke: `${(size.w / (textInputRef.current?.scrollWidth) / 6 + size.h / (textInputRef.current?.scrollHeight) / 60) / 2}px ${shapeSettings ? (shapeSettings.borderColor ? shapeSettings.borderColor : defaultSettings['SimpleText'].borderColor) : defaultSettings['SimpleText'].borderColor}`,
-        textStroke: `${(size.w / (textInputRef.current?.scrollWidth) / 6 + size.h / (textInputRef.current?.scrollHeight )/ 60) / 2}px ${shapeSettings ? (shapeSettings.borderColor ? shapeSettings.borderColor : defaultSettings['SimpleText'].borderColor) : defaultSettings['SimpleText'].borderColor}`,
+        color: isGradient ? "transparent" : shapeSettings.textColor,
+        WebkitTextStroke: isGradient
+          ? "0px transparent"
+          : `${
+              (size.w / (textInputRef.current?.scrollWidth) / 6 +
+                size.h / (textInputRef.current?.scrollHeight) / 60) /
+              2
+            }px ${
+              shapeSettings?.borderColor ||
+              defaultSettings["SimpleText"].borderColor
+            }`,
+        textStroke: isGradient
+          ? "0px transparent"
+          : `${
+              (size.w / (textInputRef.current?.scrollWidth) / 6 +
+                size.h / (textInputRef.current?.scrollHeight) / 60) /
+              2
+            }px ${
+              shapeSettings?.borderColor ||
+              defaultSettings["SimpleText"].borderColor
+            }`,
       }}
     >
       {displayText}
@@ -96,42 +119,79 @@ const SimpleText = ({size, shapeSettings}) => {
   );
 
   return (
-    <div
-      style={{
-        width: size.w,
-        height: size.h,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {shapeSettings.textAnimation === 'shake' ? (
-        <motion.div
-        key={speed}
-          style={{
-            position: "relative"
-          }}
-          {...getAnimationStyle()}
-        >
-          {scaledText}
-        </motion.div>
-      ) : (
-        <motion.div
-        key={speed}
-          style={{
-            position: "relative",
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-          {...getAnimationStyle()}
-        >
-          {scaledText}
-        </motion.div>
-      )}
-    </div>
-  )
-}
+    <>
+      <div
+        style={{
+          width: size.w,
+          height: size.h,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {shapeSettings.textAnimation === "shake" ? (
+          <motion.div key={speed} style={{ position: "relative" }} {...getAnimationStyle()}>
+            {scaledText}
+          </motion.div>
+        ) : shapeSettings.textAnimation === "gradient" ? (
+          <div className="gradient-wrapper">{scaledText}</div>
+        ) : (
+          <motion.div
+            key={speed}
+            style={{
+              position: "relative",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            {...getAnimationStyle()}
+          >
+            {scaledText}
+          </motion.div>
+        )}
+      </div>
+
+      {/* Gradient Text Animation Styles */}
+      <style>{`
+        .gradient-wrapper {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+        }
+
+        .gradient-text {
+          background: linear-gradient(
+            to right,
+            hsl(0, 0%, 100%),
+            hsl(180, 100%, 50%),
+            hsl(240, 100%, 50%),
+            hsl(270, 100%, 50%),
+            hsl(330, 40%, 70%),
+            hsl(0, 100%, 50%),
+            hsl(60, 100%, 50%),
+            hsl(90, 100%, 75%),
+            hsl(0, 0%, 100%)
+          );
+          background-size: 900% auto;
+          animation: gradientShift 6s linear infinite;
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+        }
+
+        @keyframes gradientShift {
+          0% {
+            background-position: 0% 50%;
+          }
+          100% {
+            background-position: 100% 50%;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
 
 export default SimpleText;
